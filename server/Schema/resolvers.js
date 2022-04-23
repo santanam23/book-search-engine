0eac21c42ab299cb91efb0fc77b4ad1996c1be4f
santanam23 <mirmir11.ms@gmail.com>
2022-04-23 11:58:05 -0500

    creating a Schema

--- server/Schema/resolvers.js
+++ server/Schema/resolvers.js
@@ -6,14 +6,56 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User. findOne
+              const userData = await User.findOne({ _id: context.user._id })
+                .select("-__v -password")
+                .populate("books");
+      
+              return userData;
             }
-        }
-
+            throw new AuthenticationError("Not logged in");
+          },
+          users: async () => {
+            return User.find()
+              .select("-__v -password")
+              .populate("books");
+          },
+          user: async (parent, { username }) => {
+            return User.findOne({ username })
+              .select("-__v -password")
+              .populate("books");
+          },
+          books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+          },
+          book: async (parent, { _id }) => {
+            return Book.findOne({ _id });
+          },
     },
 
     Mutation: {
-
+        addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+    
+        return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+    
+        if (!user) {
+            throw new AuthenticationError("Incorrect credentials");
+        }
+    
+        const correctPw = await user.isCorrectPassword(password);
+    
+        if (!correctPw) {
+            throw new AuthenticationError("Incorrect credentials");
+        }
+    
+        const token = signToken(user);
+        return { token, user };
+        },
     }
 }
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,10 +5,15 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -30,10 +35,5 @@ module.exports = {
 
     // send to next endpoint
     next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
