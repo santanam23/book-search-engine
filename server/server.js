e28472a9532c887257f090610aefad4e7c71d417
santanam23 <mirmir11.ms@gmail.com>
2022-04-25 10:46:46 -0500

    adding mutation routes

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // serve up react front-end in production
-router.use((req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
